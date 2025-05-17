11b29b4bed886a8f99b78c372e6b5a95e7d8517b
Naman Bagadia <namanbagadia2@gmail.com>
2025-05-17 14:59:47 +0530

    Update humeproject.py

--- humeproject.py
+++ humeproject.py
@@ -7,7 +7,7 @@ async def main() -> None:
         HUME_API_KEY = "qLv805FNRT6HzK2tHwdg89RwW1UpsXHOxgVw8jyoujZvOihU"
         client = HumeVoiceClient(HUME_API_KEY)
 
-        async with client.connect(config_id="f692be0b-5929-4bb7-8675-2d988bff4432") as socket:
+        async with client.connect(config_id="681f31b0-3735-4297-8664-7510563f09d2") as socket:
             mic_interface = MicrophoneInterface()
             await mic_interface.start(socket, allow_user_interrupt=True)
     except Exception as e:
